00aec70774764ca26d5533c1f8af6fe7ff5007f0
prgrmz07 <prgrmz07@163.com>
2024-04-25 20:15:02 +0800

    更新 qemu/script/rebuild-v5.0.0--disable-tcg-interpreter.sh

--- qemu/script/rebuild-v5.0.0--disable-tcg-interpreter.sh
+++ qemu/script/rebuild-v5.0.0--disable-tcg-interpreter.sh
@@ -1,5 +1,5 @@
 #编译步骤
-buildDir="/app/qemu/build-v8.2.2" && \
+buildDir="/app/qemu/build-v5.0.0" && \
 rm -fr $buildDir && mkdir $buildDir && cd $buildDir && \
 #  以下三行为编译步骤
 ../configure --target-list=i386-linux-user,i386-softmmu,x86_64-softmmu --disable-tcg-interpreter --enable-tcg && \ 
