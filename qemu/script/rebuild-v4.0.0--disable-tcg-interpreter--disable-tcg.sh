50bae1e82bc595afd589a4eb7c8b9af3133ea82a
prgrmz07 <prgrmz07@163.com>
2024-04-25 19:18:14 +0800

    添加 qemu/script/rebuild-v4.0.0--disable-tcg-interpreter--disable-tcg.sh

---
+++ qemu/script/rebuild-v4.0.0--disable-tcg-interpreter--disable-tcg.sh
@@ -0,0 +1,12 @@
+#编译步骤
+buildDir="/app/qemu/build-v4.0.0" && \
+rm -fr $buildDir && mkdir $buildDir && cd $buildDir && \
+#  以下三行为编译步骤
+../configure --target-list=i386-linux-user,i386-softmmu,x86_64-softmmu --disable-tcg-interpreter  --enable-debug-tcg && \ 
+make -j4
+# make install
+
+
+
+#*-linux-user : 用户态模拟，  系统调用转发给物理宿主机操作系统
+#*-softmmu    ：全系统模拟 
