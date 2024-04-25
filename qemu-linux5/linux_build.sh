25f579d6e8ae4ce4da8f68e2c8f3122621fdceae
prgrmz07 <prgrmz07@163.com>
2024-04-25 18:38:22 +0800

    更新 qemu-linux5/linux_build.sh

--- qemu-linux5/linux_build.sh
+++ qemu-linux5/linux_build.sh
@@ -4,3 +4,7 @@ make clean && \
 make ARCH=i386 CC=gcc defconfig && \
 make ARCH=i386 CC=gcc menuconfig && \
 make ARCH=i386 CC=gcc -j 6  V=1
+
+
+#defconfig==default config==默认配置
+#若去掉 menuconfig 不会弹出修改配置菜单 
