d6c71340617e69d7d15cafa3cb92858b639dbebc
prgrmz07 <prgrmz07@163.com>
2024-04-25 23:09:24 +0800

    添加 qemu-linux5/initRamFs_create.sh

---
+++ qemu-linux5/initRamFs_create.sh
@@ -0,0 +1,18 @@
+Hm=/bal/linux-stable/initRamFsHome/
+mkdir $Hm && cd $Hm
+
+wget https://www.busybox.net/downloads/binaries/1.16.1/busybox-i686
+chmod +x busybox-i686
+
+wget http://giteaz:3000/bal/bal/raw/branch/fridaAnlzAp/app/qemu-linux4/bldLinux4RunOnBochs/init
+chmod +x init
+
+# 执行 cpio_gzip 以 生成 initRamFS
+initrdF=$(pwd)/initramfs-busybox-i686.cpio.tar.gz
+RT=initramfs && \
+( rm -frv $RT &&   mkdir $RT && \
+mkdir -pv $RT/{bin,sbin,etc,proc,sys,dev} && \
+cp busybox-i686 init $RT/ &&  cd $RT  && \
+# 创建 initrd
+{ find . | cpio --create --format=newc   | gzip -9 > $initrdF ; }  ) && \
+ls -lh $initrdF
