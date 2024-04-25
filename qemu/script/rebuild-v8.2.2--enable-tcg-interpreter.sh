c72a69ac767f503f18101afeb1e26dd8c876b84b
prgrmz07 <prgrmz07@163.com>
2024-04-25 14:43:46 +0800

    添加 qemu/script/rebuild-v8.2.2--enable-tcg-interpreter.sh

---
+++ qemu/script/rebuild-v8.2.2--enable-tcg-interpreter.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+rm -fr /app/qemu/build-v8.2.2;   
+mkdir /app/qemu/build-v8.2.2; 
+cd /app/qemu/build-v8.2.2;   
+../configure --target-list=i386-softmmu,x86_64-softmmu --enable-tcg-interpreter --enable-tcg && \
+make -j4 && \
+#make install && \
+ln -s /app/qemu/build-v8.2.2 /app/qemu/build-v8.2.2-modify
