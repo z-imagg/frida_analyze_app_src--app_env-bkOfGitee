4c4ebb4ef1a5231899bb351698069a9fd3a50900
prgrmz07 <prgrmz07@163.com>
2024-04-25 15:05:48 +0800

    更新 qemu/script/rebuild-v8.2.2--disable-tcg-interpreter.sh

--- qemu/script/rebuild-v8.2.2--disable-tcg-interpreter.sh
+++ qemu/script/rebuild-v8.2.2--disable-tcg-interpreter.sh
@@ -3,7 +3,7 @@
 rm -fr /app/qemu/build-v8.2.2;   
 mkdir /app/qemu/build-v8.2.2; 
 cd /app/qemu/build-v8.2.2;   
-../configure --target-list=i386-softmmu,x86_64-softmmu --disable-tcg-interpreter --enable-tcg && \
+../configure --target-list=i386-softmmu,x86_64-softmmu --disable-tcg-interpreter --enable-tcg --enable-debug-info && \
 make -j4 && \
 #make install && \
 ln -s /app/qemu/build-v8.2.2 /app/qemu/build-v8.2.2-modify
