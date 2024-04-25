7672c82c5ff1a2bc24ce725ed9c01f3dea49ad8a
prgrmz07 <prgrmz07@163.com>
2024-04-25 18:52:03 +0800

    更新 qemu-linux5/linux_build.sh

--- qemu-linux5/linux_build.sh
+++ qemu-linux5/linux_build.sh
@@ -8,3 +8,8 @@ make ARCH=i386 CC=gcc -j 6  V=1
 
 #defconfig==default config==默认配置
 #若去掉 menuconfig 不会弹出修改配置菜单 
+
+
+#linux-5.0 的 menuconfig:
+#General --> Configure standard kernel features(expert users) --> Include all symbols in kallsyms
+#kernel hacking --> Kernel debugging
