47f6c082897fe92df8f37a1820b796ce83aabd44
prgrmz07 <prgrmz07@163.com>
2024-04-27 00:30:12 +0800

    ubuntu16.04 linux5 build env (docker)

--- linux5/main_dockerImage_build_run.sh
+++ linux5/main_dockerImage_build_run.sh
@@ -19,11 +19,11 @@ source /bal/bash-simplify/cdCurScriptDir.sh && cdCurScriptDir
 
 #构建基础镜像 
 #  转换 ase_ubuntu_16.04.Dockerfile.sh ---> ase_ubuntu_16.04.Dockerfile  、 删除 、 构建docker镜像
-convert_sh_to_Dockerfile__rmInst__rmImage    ubuntu1604Lnx5Bld 0.1  ;  docker build --progress=plain --no-cache  -f "ubuntu1604Lnx5Bld.Dockerfile" -t ubuntu1604Lnx5Bld:0.1 "/" 
+convert_sh_to_Dockerfile__rmInst__rmImage    ubuntu1604_lnx5bld 0.1  ;  docker build --progress=plain --no-cache  -f "ubuntu1604_lnx5bld.Dockerfile" -t ubuntu1604_lnx5bld:0.1 "/" 
 
 
 # 若初次启动时，则 克隆项目代码 并 退出
-docker run --privileged=true  --name ubuntu1604Lnx5Bld --hostname ubuntu1604Lnx5Bld -it ubuntu1604Lnx5Bld:0.1
+docker run --privileged=true  --name ubuntu1604_lnx5bld --hostname ubuntu1604_lnx5bld -it ubuntu1604_lnx5bld:0.1
 # 退出后，再次启动
-docker start ubuntu1604Lnx5Bld
-docker exec -it ubuntu1604Lnx5Bld  /usr/bin/bash
+docker start ubuntu1604_lnx5bld
+docker exec -it ubuntu1604_lnx5bld  /usr/bin/bash
