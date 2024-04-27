251feffaaacd36a448f26bcb03fdfe665e2dde3d
prgrmz07 <prgrmz07@163.com>
2024-04-27 16:59:44 +0800

    apt install安静一点：丢弃标准输出 1>/dev/null 但保留标准错误输出、 子shenll进程中繁琐模式( set -x ; ... ; )

--- linux5/ubuntu1604_lnx5bld.Dockerfile.sh
+++ linux5/ubuntu1604_lnx5bld.Dockerfile.sh
@@ -10,23 +10,26 @@
 
 #dk# RUN bash -c ''' \
 { \
-apt-get update && \
-apt-get install -y  build-essential && \
+ArgAptGet="-qq   -y" && \
+# 在子shell进程开启繁琐模式 即 (set -x ; ... ;)
+( set -x && \
+apt-get $ArgAptGet update && \
+apt-get $ArgAptGet install  build-essential 1>/dev/null && \
 #libncurses5-dev被menuconfig需要
-apt-get install -y libncurses5-dev && \
+apt-get $ArgAptGet install libncurses5-dev 1>/dev/null && \
 #解决报错: 找不到 flex、找不到 bison
 #  flex bison 被linux-5.0需要,  但linux-4.14-y不需要
-apt-get install -y flex bison && \
+apt-get $ArgAptGet install flex bison 1>/dev/null && \
 #解决报错: 找不到 ssl/bio.h
-apt-get install -y libssl-dev && \
+apt-get $ArgAptGet install libssl-dev 1>/dev/null && \
 #解决报错: 找不到bc
-apt-get install -y bc && \
+apt-get $ArgAptGet install bc 1>/dev/null && \
 ##解决报错
-apt-get install -y libelf-dev  && \
+apt-get $ArgAptGet install libelf-dev  1>/dev/null && \
 #工具
-apt-get install -y git file cpio wget curl && \
+apt-get $ArgAptGet install git file cpio wget curl 1>/dev/null && \
 # gcc (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609
-gcc --version  && \
+gcc --version ;) && \
 true ;} \
 || false #dk# '''
 
