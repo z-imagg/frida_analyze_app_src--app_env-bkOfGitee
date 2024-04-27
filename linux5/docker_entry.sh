ec5bb611e08449e3599ea5f95e4b4586be9517c2
prgrmz07 <prgrmz07@163.com>
2024-04-27 14:43:53 +0800

    非初次启动 == git仓库目录存在且当前提交在给定远程标签

--- linux5/docker_entry.sh
+++ linux5/docker_entry.sh
@@ -8,12 +8,16 @@
 #此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
 set -e
 
+source  <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/git_switch_to_remote_tag.sh)
+
+LnxVer="v5.11"
+dkLnxRpD="/app/linux"
 
 #docker实例运行的时候，才克隆项目代码，方便docker image上传到dockerhub ，同时也不泄漏项目源码
 # 若初次启动时，则 克隆项目代码 并 退出
-[[ -f /app/linux/.git/config ]] || { bash -x /fridaAnlzAp/app_qemu/app_bld/linux5/init_proj.sh && exit 0 ;}
+git_switch_to_remote_tag "$dkLnxRpD" "$LnxVer" 2>/dev/null || { bash -x /fridaAnlzAp/app_qemu/app_bld/linux5/init_proj.sh && exit 0 ;}
 
 # 若非初次启动，则启动bash
-cd /app/linux/ && /usr/bin/bash
+cd $dkLnxRpD && /usr/bin/bash
 
 
