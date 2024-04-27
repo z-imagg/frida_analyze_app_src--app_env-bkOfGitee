3733c2c9e41eb98960087e0bab9022023621a0bf
prgrmz07 <prgrmz07@163.com>
2024-04-27 17:23:30 +0800

    ubuntu1604_lnx5bld -->  ubuntu1604_linux5build; 提变量

--- linux5/main_dockerImage_build_run.sh
+++ linux5/main_dockerImage_build_run.sh
@@ -18,8 +18,10 @@ source /bal/bash-simplify/cdCurScriptDir.sh && cdCurScriptDir
 
 
 #构建基础镜像 
-#  转换 ase_ubuntu_16.04.Dockerfile.sh ---> ase_ubuntu_16.04.Dockerfile  、 删除 、 构建docker镜像
-convert_sh_to_Dockerfile__rmInst__rmImage    ubuntu1604_lnx5bld 0.1  ;  docker build --progress=plain --no-cache  -f "ubuntu1604_lnx5bld.Dockerfile" -t ubuntu1604_lnx5bld:0.1 "/" 
+#  转换 ubuntu1604_linux5build.Dockerfile.sh ---> ubuntu1604_linux5build.Dockerfile  、 删除 、 构建docker镜像
+dkInstName="ubuntu1604_linux5build"
+dkInstVer="0.1"
+convert_sh_to_Dockerfile__rmInst__rmImage    $dkInstName $dkInstVer  ;  docker build --progress=plain --no-cache  -f "$dkInstName.Dockerfile" -t $dkInstName:$dkInstVer "/" 
 
 # 宿主机是否有linux仓库
 hostLnxRpD=/bal/linux-stable
@@ -31,7 +33,7 @@ volMap=""
 # 若 宿主机有linux仓库， 则 docker实例映射该目录
 $hostHasLnxRp && volMap="$volMap --volume $hostLnxRpD:$dkLnxRpD"
 # 若初次启动时，则 克隆项目代码 并 退出
-docker run -e isDkInstInit='true' $volMap  --name ubuntu1604_lnx5bld --hostname ubuntu1604_lnx5bld -it ubuntu1604_lnx5bld:0.1
+docker run -e isDkInstInit='true' $volMap  --name $dkInstName --hostname $dkInstName -it $dkInstName:$dkInstVer
 # 退出后，再次启动
-# docker start ubuntu1604_lnx5bld
-echo "enter work bash: 'docker exec -it ubuntu1604_lnx5bld  /usr/bin/bash'"
+# docker start $dkInstName
+echo "enter work bash: 'docker exec -it $dkInstName  /usr/bin/bash'"
