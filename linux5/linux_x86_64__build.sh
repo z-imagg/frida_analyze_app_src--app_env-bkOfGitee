801ba518f8016b82c6097cc1fac0f727574e9328
prgrmz07 <prgrmz07@163.com>
2024-04-27 19:25:30 +0800

    首次nconfig手工配置 并保存为 预设配置文件 ， 下次 无需nconfig 而 直接使用该 预设配置文件

--- linux5/linux_x86_64__build.sh
+++ linux5/linux_x86_64__build.sh
@@ -6,11 +6,29 @@
 #【备注】   
 #【运行环境】 
 
+msg_tip="""
+无预设配置文件.config , 
+需以nconfig手工选填配置文件以生成配置文件/app/linux/.config ,参考此文注释部分: http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/linux5/linux_x86_64__build.sh
+按回车继续:
+"""
+
+dfltCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.default"
+myCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.pvh__debug"
+objCfgF="/app/linux/.config"
+#是否有预设配置文件 .config.pvh__debug
+HasMyCfg=false; [[ -f $myCfgF ]] && HasMyCfg=true
+#【取反】NoMyCfg==not HasMyCfg
+NoMyCfg=true; $HasMyCfg && NoMyCfg=false
+
 cd /app/linux/ && \
 make mrproper && \
 make clean && \
-make ARCH=x86_64 CC=gcc defconfig && \
-make ARCH=x86_64 CC=gcc nconfig && \
+#首次 nconfig手工配置 并保存为 预设配置文件 ， 下次 无需nconfig 而 直接使用该 预设配置文件
+# 【下次】若有预设配置文件 , 则使用之
+{ $HasMyCfg && cp -v $myCfgF   $objCfgF ; \
+# 【首次】若无预设配置文件 , 则以nconfig手工选填配置文件 并 保存之供给下次用（下次无需手工nconfig）
+$NoMyCfg  && read -p "$msg_tip" &&  ( make ARCH=x86_64 CC=gcc defconfig && cp -v $objCfgF $dfltCfgF && make ARCH=x86_64 CC=gcc nconfig && cp -v $objCfgF $myCfgF ;) ; \
+true ;} && \
 make ARCH=x86_64 CC=gcc -j 6  V=1
 
 #nconfig 和 menuconfig 都是 文本界面, 但 nconfig更好操作一些
