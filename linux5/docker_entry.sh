6698f01148dfa822f4fe65e4184a089f1b1ad226
prgrmz07 <prgrmz07@163.com>
2024-04-27 14:25:18 +0800

    debug init_proj.sh

--- linux5/docker_entry.sh
+++ linux5/docker_entry.sh
@@ -11,7 +11,7 @@ set -e
 
 #docker实例运行的时候，才克隆项目代码，方便docker image上传到dockerhub ，同时也不泄漏项目源码
 # 若初次启动时，则 克隆项目代码 并 退出
-[[ -f /app/linux/.git/config ]] || { bash +x /fridaAnlzAp/app_qemu/app_bld/linux5/init_proj.sh && exit 0 ;}
+[[ -f /app/linux/.git/config ]] || { bash -x /fridaAnlzAp/app_qemu/app_bld/linux5/init_proj.sh && exit 0 ;}
 
 # 若非初次启动，则启动bash
 cd /app/linux/ && /usr/bin/bash
