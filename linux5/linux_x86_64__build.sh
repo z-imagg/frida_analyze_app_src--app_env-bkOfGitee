b36c49ff4cec36efb32caa65a74a177c945f8199
prgrmz07 <prgrmz07@163.com>
2024-04-27 18:44:39 +0800

    微调  'linux-5.0 的 nconfig说明 '

--- linux5/linux_x86_64__build.sh
+++ linux5/linux_x86_64__build.sh
@@ -20,11 +20,11 @@ make ARCH=x86_64 CC=gcc -j 6  V=1
 
 
 #linux-5.0 的 nconfig: 
-# 试图启用调试
-#   kernel hacking --> Compile-time checks and compiler options --> Compile the kernel with debug info
-#   General --> Configure standard kernel features(expert users) --> Include all symbols in kallsyms
-#   kernel hacking --> Kernel debugging
 # 启用PVH:
 #   Processor type and features --> Linux guest support  --> Support for running PVH guests
+# 试图启用调试
+#   kernel hacking --> Compile-time checks and compiler options --> Compile the kernel with debug info
+#   kernel hacking --> Kernel debugging (默认已启用)
+#   General  --> Include all symbols in kallsyms
 
 #nconfig 生成的配置保存在文件.config中,  mrproper && clean 会删除 .config文件
