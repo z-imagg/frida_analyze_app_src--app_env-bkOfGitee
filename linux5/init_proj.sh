e9c2039983cfac01cb459c1ae720fab0af607b5e
prgrmz07 <prgrmz07@163.com>
2024-04-27 17:15:35 +0800

    微调

--- linux5/init_proj.sh
+++ linux5/init_proj.sh
@@ -39,9 +39,9 @@ git_switch_to_remote_tag $dkLnxRpD v5.11
 
 #docker首次运行，初始化完毕后 显示相关目录
 # 存在的路径
-ls /   /app /tmp_fridaAnlzAp/ /fridaAnlzAp/      /app/linux/.git/config    
+echo "告知存在的路径" ; ls /   /app /tmp_fridaAnlzAp/ /fridaAnlzAp/      /app/linux/.git/config    
 # 不存在的路径
-echo "只是告知这些路径不存在,非错误" ; ls /app/bash-simplify/.git  /tmp_fridaAnlzAp/prj_env/.git /tmp_fridaAnlzAp/app_qemu/.git || true
+echo "告知不存在路径,非错误" ; ls /app/bash-simplify/.git  /tmp_fridaAnlzAp/prj_env/.git /tmp_fridaAnlzAp/app_qemu/.git || true
 
 #   #endregion
 
