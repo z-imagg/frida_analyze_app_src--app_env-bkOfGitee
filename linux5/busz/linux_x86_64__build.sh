8bf9b1754b76b21c8df02fa74d2492c5ee967b2c
prgrmz07 <prgrmz07@163.com>
2024-04-29 00:32:14 +0800

    整理linux_x86_64__build.sh

--- linux5/busz/linux_x86_64__build.sh
+++ linux5/busz/linux_x86_64__build.sh
@@ -9,30 +9,44 @@
 #此脚本任何语句 退出代码不为正常值0 ，都会导致整个脚本退出
 set -e
 
-msg_tip="""
+
+
+#调用nconfig手工配置获得.config， 或者用现成的.config
+function getConfig() {
+local msg_tip="""
 无预设配置文件.config , 
 需以nconfig手工选填配置文件以生成配置文件/app/linux/.config ,参考此文注释部分: http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/linux5/busz/linux_x86_64__build.sh
 按回车继续:
 """
 
-dfltCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.default"
-myCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.pvh__debug"
-objCfgF="/app/linux/.config"
+local dfltCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.default"
+local myCfgF="/fridaAnlzAp/app_qemu/app_bld/linux5/.config.pvh__debug"
+local objCfgF="/app/linux/.config"
 #是否有预设配置文件 .config.pvh__debug
-HasMyCfg=false; [[ -f $myCfgF ]] && HasMyCfg=true
+local HasMyCfg=false; [[ -f $myCfgF ]] && HasMyCfg=true
 #【取反】NoMyCfg==not HasMyCfg
-NoMyCfg=true; $HasMyCfg && NoMyCfg=false
+local NoMyCfg=true; $HasMyCfg && NoMyCfg=false
 
-cd /app/linux/ && \
-make mrproper && \
-make clean && \
 #首次 nconfig手工配置 并保存为 预设配置文件 ， 下次 无需nconfig 而 直接使用该 预设配置文件
 # 【下次】若有预设配置文件 , 则使用之
 { $HasMyCfg && cp -v $myCfgF   $objCfgF ; \
 # 【首次】若无预设配置文件 , 则以nconfig手工选填配置文件 并 保存之供给下次用（下次无需手工nconfig）
 $NoMyCfg  && read -p "$msg_tip" &&  ( make ARCH=x86_64 CC=gcc defconfig && cp -v $objCfgF $dfltCfgF && make ARCH=x86_64 CC=gcc nconfig && cp -v $objCfgF $myCfgF ;) ; \
-true ;} && \
+true ;}
+
+}
+
+#编译步骤
+cd /app/linux/ && \
+make mrproper && \
+make clean && \
+getConfig && \
 make ARCH=x86_64 CC=gcc -j 6  V=1
+
+
+#编译产物展示
+function printLinuxKernel() {
+cd /app/linux/ 
 
 ls -lh  vmlinux
 # -rwxr-xr-x   22M  vmlinux
@@ -44,6 +58,10 @@ ls -lh arch/x86/boot/bzImage
 # -rw-r--r--   7.5M   arch/x86/boot/bzImage
 
 file arch/x86/boot/bzImage 
+}
+
+#编译产物展示
+printLinuxKernel
 
 #####以下是文档
 
