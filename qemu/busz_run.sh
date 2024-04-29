741f0828270639e20cc7998af2d3f6c80c8f39b1
prgrmz07 <prgrmz07@163.com>
2024-04-29 18:03:36 +0800

    调试

--- qemu/busz_run.sh
+++ qemu/busz_run.sh
@@ -2,6 +2,6 @@
 
 #执行业务脚本
 # 业务脚本具有幂等性
-bash  /fridaAnlzAp/app_qemu/app_bld/qemu/busz/rebuild-qemu-v5.0.0--disable-tcg-interpreter--disable-tcg.sh
-bash  /fridaAnlzAp/app_qemu/app_bld/qemu/busz/qemu_boot_vmlinux.sh
+bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/rebuild-qemu-v5.0.0--disable-tcg-interpreter--disable-tcg.sh
+bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/qemu_boot_vmlinux.sh
 
