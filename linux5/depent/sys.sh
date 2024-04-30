6e7689caeefb8aa2bf463f312b1620454557a148
prgrmz07 <prgrmz07@163.com>
2024-05-01 00:28:14 +0800

    linux5环境： Dockerfile中的脚本的sudo处理

--- linux5/depent/sys.sh
+++ linux5/depent/sys.sh
@@ -9,21 +9,23 @@
 ArgAptGet="-qq   -y" && \
 # 在子shell进程开启繁琐模式 即 (set -x ; ... ;)
 ( set -x && \
-apt-get $ArgAptGet update && \
-apt-get $ArgAptGet install  build-essential 1>/dev/null && \
+SUDO="sudo" ; { [[ $(whoami) == root ]] && SUDO="" ;} && \
+$SUDO apt-get $ArgAptGet update && \
+$SUDO apt-get $ArgAptGet install -y  sudo && \
+sudo apt-get $ArgAptGet install  build-essential 1>/dev/null && \
 #libncurses5-dev被menuconfig需要
-apt-get $ArgAptGet install libncurses5-dev 1>/dev/null && \
+sudo apt-get $ArgAptGet install libncurses5-dev 1>/dev/null && \
 #解决报错: 找不到 flex、找不到 bison
 #  flex bison 被linux-5.0需要,  但linux-4.14-y不需要
-apt-get $ArgAptGet install flex bison 1>/dev/null && \
+sudo apt-get $ArgAptGet install flex bison 1>/dev/null && \
 #解决报错: 找不到 ssl/bio.h
-apt-get $ArgAptGet install libssl-dev 1>/dev/null && \
+sudo apt-get $ArgAptGet install libssl-dev 1>/dev/null && \
 #解决报错: 找不到bc
-apt-get $ArgAptGet install bc 1>/dev/null && \
+sudo apt-get $ArgAptGet install bc 1>/dev/null && \
 ##解决报错
-apt-get $ArgAptGet install libelf-dev  1>/dev/null && \
+sudo apt-get $ArgAptGet install libelf-dev  1>/dev/null && \
 #工具
-apt-get $ArgAptGet install git file cpio wget curl rsync sudo 1>/dev/null && \
+sudo apt-get $ArgAptGet install git file cpio wget curl rsync   1>/dev/null && \
 # gcc (Ubuntu 5.4.0-6ubuntu1~22.04.12) 5.4.0 20160609
 gcc --version ;) && \
 true
