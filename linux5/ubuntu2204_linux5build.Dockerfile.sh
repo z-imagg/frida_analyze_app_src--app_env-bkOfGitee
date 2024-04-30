366b19a22feda59df4a7378b520d61180bf47b65
prgrmz07 <prgrmz07@163.com>
2024-04-30 23:50:06 +0800

    linux5环境： Dockerfile中的脚本转移到*.sh

--- linux5/ubuntu2204_linux5build.Dockerfile.sh
+++ linux5/ubuntu2204_linux5build.Dockerfile.sh
@@ -8,29 +8,16 @@
 #dk# COPY /fridaAnlzAp/app_qemu/app_bld /fridaAnlzAp/app_qemu/app_bld
 #dk# COPY /app/bash-simplify/local_domain_set.sh /app/bash-simplify/local_domain_set.sh
 #dk# COPY /fridaAnlzAp/app_qemu/app_bld/linux5/busz_run.sh /root/busz_run.sh
+#dk# COPY /fridaAnlzAp/app_qemu/app_bld/linux5/depent /fridaAnlzAp/app_qemu/app_bld/linux5/depent
 
 #dk# RUN bash -c ''' \
 { \
-ArgAptGet="-qq   -y" && \
-# 在子shell进程开启繁琐模式 即 (set -x ; ... ;)
-( set -x && \
-apt-get $ArgAptGet update && \
-apt-get $ArgAptGet install  build-essential 1>/dev/null && \
-#libncurses5-dev被menuconfig需要
-apt-get $ArgAptGet install libncurses5-dev 1>/dev/null && \
-#解决报错: 找不到 flex、找不到 bison
-#  flex bison 被linux-5.0需要,  但linux-4.14-y不需要
-apt-get $ArgAptGet install flex bison 1>/dev/null && \
-#解决报错: 找不到 ssl/bio.h
-apt-get $ArgAptGet install libssl-dev 1>/dev/null && \
-#解决报错: 找不到bc
-apt-get $ArgAptGet install bc 1>/dev/null && \
-##解决报错
-apt-get $ArgAptGet install libelf-dev  1>/dev/null && \
-#工具
-apt-get $ArgAptGet install git file cpio wget curl rsync sudo 1>/dev/null && \
-# gcc (Ubuntu 5.4.0-6ubuntu1~22.04.12) 5.4.0 20160609
-gcc --version ;) && \
+#本地域名总是要设置的
+source /fridaAnlzAp/app_qemu/app_bld/util/LocalDomainSet.sh && \
+#导入_importBSFn.sh
+source /fridaAnlzAp/app_qemu/app_bld/util/Load__importBSFn.sh && \
+#安装系统依赖包
+bash -x /fridaAnlzAp/app_qemu/app_bld/linux5/depent/sys.sh && \
 true ;} \
 || false #dk# '''
 
