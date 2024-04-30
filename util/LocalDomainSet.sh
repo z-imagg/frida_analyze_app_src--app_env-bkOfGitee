09599193320371e900361665fa1fb0296ffeaae2
prgrmz07 <prgrmz07@163.com>
2024-04-30 16:42:54 +0800

    跟随_importBSFn

--- util/LocalDomainSet.sh
+++ util/LocalDomainSet.sh
@@ -9,8 +9,9 @@
 
 function LocalDomainSet() {
 
+{ source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/_importBSFn.sh) && \
 #若设置本地域名失败，则返回异常（退出代码27）
-( _importBSFn local_domain_set.sh && local_domain_set ;) || return 27
+( _importBSFn local_domain_set.sh && local_domain_set ;) ;} || return 27
 
 
 }
