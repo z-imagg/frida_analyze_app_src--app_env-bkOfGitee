88a5d910e17bc9fb026de0c4b5c9ccc9f1f6f919
prgrmz07 <prgrmz07@163.com>
2024-04-30 15:22:14 +0800

    gitproxy_Clone_SwitchTag、git_Clone_SwitchTag 迁移到bash-siimplify

--- util/LocalDomainSet.sh
+++ util/LocalDomainSet.sh
@@ -6,10 +6,11 @@
 #【备注】   
 
 
+
 function LocalDomainSet() {
 
 #若设置本地域名失败，则返回异常（退出代码27）
-( source  /app/bash-simplify/local_domain_set.sh && local_domain_set ;) || return 27
+( source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/local_domain_set.sh) && local_domain_set ;) || return 27
 
 
 }
