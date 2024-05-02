02495f3fb866e3f13558582fb9e0b62791fa8f74
prgrmz07 <prgrmz07@163.com>
2024-05-02 14:29:08 +0800

    $pdir==/fridaAnlzAp/app_bld/

--- .gen_toc.sh
+++ .gen_toc.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source <(curl http://giteaz:3000/bal/bash-simplify/raw/tag/tag_release/gen_toc.sh)
+#进入目录 当前目录, 变量pdir为当前目录绝对路径
+source ./pre_init.sh || exit 70
+
+source $bash_simplify/gen_toc.sh
 # 【用法举例】 gen_tableOfContent  /app/wiki/ http://giteaz:3000/wiki/wiki/src/branch/main /app/wiki/readme.md
 
 gen_tableOfContent  $pdir/ http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/main $pdir/readme.md 
