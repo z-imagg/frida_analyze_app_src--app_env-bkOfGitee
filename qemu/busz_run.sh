f738818ceecd3352f34b1c55a83003444dc638f9
prgrmz07 <prgrmz07@163.com>
2024-05-02 14:24:09 +0800

    暂时删除qemu

--- qemu/busz_run.sh
+++
@@ -1,7 +0,0 @@
-
-
-#执行业务脚本
-# 业务脚本具有幂等性
-bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/rebuild-qemu-v8.2.2--disable-tcg-interpreter--disable-tcg.sh
-bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/qemu_boot_vmlinux.sh
-
