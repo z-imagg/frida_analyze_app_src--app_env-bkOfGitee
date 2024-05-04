7129246abb87c2dfd27b36f4a275eac020652ac9
prgrmz07 <prgrmz07@163.com>
2024-05-04 23:54:54 +0800

    中性模板 2

--- manual_txt.sh
+++ manual_txt.sh
@@ -9,29 +9,9 @@
 source $pdir/docker_instance.sh
 
 manual_txt="""使用说明
-
-1. linux5编译
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
-  来自:   http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/linux5/manual_txt.sh
-
-1.1. 进入 docker实例 ${dkInstName} 的bash终端: 
-  exit #退出此时的docker实例终端
-  docker start  $dkInstName ; docker exec -it $dkInstName  /bin/bash
-
-1.2. 编译linux:
-  bash -x $pdir/busz/linux_x86_64__build.sh
-
-1.3. menuconfig配置人工修改,参考此文注释部分: 
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/linux5/busz/linux_x86_64__build.sh
-
-1.4. 以busybox制作initramfs:
-  bash -x $pdir/busz/initRamFs_create.sh
-
-2. qemu编译:
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
-  来自: http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/manual_txt.sh
-  详细步骤见链接
-
+使用说明正文
+1. xxx
+2. yyy
 """
 
 
