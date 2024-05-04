a0a368eea1e8dc99d39d1a716abeb2a0bac35240
prgrmz07 <prgrmz07@163.com>
2024-05-04 19:40:53 +0800

    【草稿】直白的复制：linux-5.11  --> qemu-5.0.0
    复制自qemu5正常运行时的提交
    
    http://giteaz:3000/frida_analyze_app_src/app_bld/src/commit/263861af48bc011a95a84ef09b009288905fd37b
    
    http://giteaz:3000/frida_analyze_app_src/app_bld/src/tag/OK1_app_bld__linux_qemu

--- manual_txt.sh
+++ manual_txt.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #【描述】  使用手册文本
 #【依赖】   
@@ -6,32 +6,28 @@
 #【备注】   
 
 #定义 docker镜像、实例 的 名称、版本号
-source $pdir/docker_instance.sh
+source /fridaAnlzAp/app_qemu/app_bld/qemu/docker_instance.sh
 
 manual_txt="""使用说明
 
 1. linux5编译
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
-  来自:   http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/linux5/manual_txt.sh
+  http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
+  来自:   http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/linux5/manual_txt.sh
+  详细步骤见链接
 
-1.1. 进入 docker实例 ${dkInstName} 的bash终端: 
+2. qemu编译:
+  http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
+  来自: http://giteaz:3000/frida_analyze_app_src/app_bld/src/branch/app/qemu/qemu/manual_txt.sh
+
+2.1. 进入 docker实例 ${dkInstName} 的bash终端: 
   exit #退出此时的docker实例终端
   docker start  $dkInstName ; docker exec -it $dkInstName  /bin/bash
 
-1.2. 编译linux:
-  bash -x $pdir/busz/linux_x86_64__build.sh
+2.2. 编译qemu5.0.0:
+  bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/rebuild-qemu-v8.2.2--disable-tcg-interpreter--disable-tcg.sh
 
-1.3. menuconfig配置人工修改,参考此文注释部分: 
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/linux5/busz/linux_x86_64__build.sh
-
-1.4. 以busybox制作initramfs:
-  bash -x $pdir/busz/initRamFs_create.sh
-
-2. qemu编译:
-  http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/main_dockerImage_build_run.sh
-  来自: http://giteaz:3000/frida_analyze_app_src/app_env/src/branch/app/qemu/qemu/manual_txt.sh
-  详细步骤见链接
-
+3.3. qemu启动vmlinux:
+  bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/busz/qemu_boot_vmlinux.sh
 """
 
 
