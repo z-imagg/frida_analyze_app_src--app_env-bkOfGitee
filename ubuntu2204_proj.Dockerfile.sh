cf2502823313dd1e1a9e6e654cf1db0dbe7daf48
prgrmz07 <prgrmz07@163.com>
2024-05-04 19:54:00 +0800

    修复中4

--- ubuntu2204_proj.Dockerfile.sh
+++ ubuntu2204_proj.Dockerfile.sh
@@ -4,24 +4,30 @@
 
 #dk# FROM ubuntu:22.04 as base
 #dk# WORKDIR /
-#dk# COPY ./hostRoot/fridaAnlzAp/app_qemu /fridaAnlzAp/app_qemu
-#dk# COPY ./hostRoot/fridaAnlzAp/prj_env /fridaAnlzAp/prj_env
+#dk# COPY ./hostRoot/app/app_env /app/app_env
 #dk# COPY ./hostRoot/app/bash-simplify  /app/bash-simplify
+
+#Dockerfile: 定义 'ARG x=...' , 使用 RUN 'echo $x; bash -x f_use_x.sh'
+#dk# ARG pdir="$pdir"
 
 #dk# RUN bash -c ''' \
 { \
+#标记此时是在docker镜像构建阶段
+touch /app/app_env/flag_dockerBuildImage /app/bash-simplify/flag_dockerBuildImage && \
+#粗略显示已复制文件
+find /app/ -maxdepth 2 && \
 #安装系统依赖包
-bash -x /fridaAnlzAp/app_qemu/app_bld/qemu/depent/sys.sh && \
+bash $bsFlg $pdir/depent/sys.sh && \
 #基本需求: 域名设置、克隆基本仓库
-source /fridaAnlzAp/app_qemu/prj_env/util/basic_require.sh && \
+source  $pdir/util/basic_require.sh && \
 true ;} \
 || false #dk# '''
 
 
 #dk# LABEL maintainer="prgrmz07 <prgrmz07@163.com>"
 #dk# LABEL version="0.1"
-#dk# LABEL description="ubuntu22.04 qemu5.0.0 编译环境 docker "
+#dk# LABEL description="ubuntu22.04 proj 编译环境 docker "
 
 
 
-#dk# ENTRYPOINT ["/bin/bash", "-x", "/fridaAnlzAp/app_qemu/app_bld/qemu/docker_entry.sh" ]
+#dk# ENTRYPOINT ["/bin/bash", "$pdir/docker_entry.sh" ]
