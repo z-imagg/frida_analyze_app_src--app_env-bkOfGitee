d12d096201a2daa88df769b093c7fff3cd00c882
prgrmz07 <prgrmz07@163.com>
2024-05-05 12:06:18 +0800

    若busz目录下无脚本文件,则正常退出

--- busz_run.sh
+++ busz_run.sh
@@ -5,6 +5,9 @@
 #业务脚本们路径 读取到 BASH变量buszScriptFPath_Arr中
 mapfile -t buszScriptFPath_Arr < <(find $pdir/busz/ -type f | sort --unique )
 
+#若busz目录下无脚本文件,则正常退出
+[[ ${#buszScriptFPath_Arr} -eq 0 ]] && exit 0
+
 echo "业务脚本路径数组:【${buszScriptFPath_Arr[@]}】"
 
 #循环执行业务脚本
