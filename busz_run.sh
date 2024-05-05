5d4e559483dd9d891e13ef616ef080210105ae13
prgrmz07 <prgrmz07@163.com>
2024-05-05 13:50:52 +0800

    fix 5

--- busz_run.sh
+++ busz_run.sh
@@ -3,7 +3,7 @@
 
 
 #业务脚本们路径 读取到 BASH变量buszScriptFPath_Arr中
-mapfile -t buszScriptFPath_Arr < <(find $pdir/busz/ -type f | sort --unique )
+mapfile -t buszScriptFPath_Arr < <(find $pdir/busz/  -maxdepth 1  -and  -type f -and -name "*.sh" | sort --unique )
 
 #若busz目录下无脚本文件,则正常退出
 [[ ! -v buszScriptFPath_Arr || ${#buszScriptFPath_Arr} -eq 0 ]] && exit 0
