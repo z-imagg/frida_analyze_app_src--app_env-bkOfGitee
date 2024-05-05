11581d67ec962c018793703553392d57765f64d7
prgrmz07 <prgrmz07@163.com>
2024-05-05 14:30:32 +0800

    linux-v5.11

--- depent/sys.sh
+++ depent/sys.sh
@@ -15,6 +15,20 @@ ArgAptGet="-qq   -y" && \
 { { $isRootUsr || SUDO="sudo" ;} ; { $isRootUsr && SUDO="" ;} ; true ;} && \
 $SUDO apt-get $ArgAptGet update  1>/dev/null && \
 $SUDO apt-get $ArgAptGet install -y  sudo  1>/dev/null && \
-sudo apt-get $ArgAptGet install git curl file rsync  1>/dev/null && \
-echo "end_of_'sys.sh'" ;) && \
+sudo apt-get $ArgAptGet install  build-essential 1>/dev/null && \
+#libncurses5-dev被menuconfig需要
+sudo apt-get $ArgAptGet install libncurses5-dev 1>/dev/null && \
+#解决报错: 找不到 flex、找不到 bison
+#  flex bison 被linux-5.0需要,  但linux-4.14-y不需要
+sudo apt-get $ArgAptGet install flex bison 1>/dev/null && \
+#解决报错: 找不到 ssl/bio.h
+sudo apt-get $ArgAptGet install libssl-dev 1>/dev/null && \
+#解决报错: 找不到bc
+sudo apt-get $ArgAptGet install bc 1>/dev/null && \
+##解决报错
+sudo apt-get $ArgAptGet install libelf-dev  1>/dev/null && \
+#工具
+sudo apt-get $ArgAptGet install git file cpio wget curl rsync   1>/dev/null && \
+# gcc (Ubuntu 5.4.0-6ubuntu1~22.04.12) 5.4.0 20160609
+gcc --version ;) && \
 true
