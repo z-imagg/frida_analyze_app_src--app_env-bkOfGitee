a604ff60431782e6b1ed23b435b8f682f8376aa6
prgrmz07 <prgrmz07@163.com>
2024-05-06 17:47:53 +0800

    修改模板： 最后  启动bash（以bashrc.sh为rcfile）

--- bashrc.sh
+++ bashrc.sh
@@ -14,4 +14,5 @@ source /app/nvm/nvm.sh && \
 export NEO4J_HOME=/app/neo4j-community-4.4.32 && \
 export JAVA_HOME=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 && \
 export PATH=$PATH:$NEO4J_HOME/bin:$JAVA_HOME/bin && \
+alias cytoscape_boot_with_java17='JAVA_HOME=/app/zulu17.48.15-ca-jdk17.0.10-linux_x64/ /app/cytoscape-unix-3.10.2/cytoscape.sh' && \
 echo $OkEnvMsg
