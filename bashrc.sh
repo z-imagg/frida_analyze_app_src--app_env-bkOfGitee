6cb84d4d956f3ad60ff735b5e6e5dc31238c60ea
prgrmz07 <prgrmz07@163.com>
2024-05-06 18:08:00 +0800

    修改模板： 最后  启动bash（以bashrc.sh为rcfile）

--- bashrc.sh
+++ bashrc.sh
@@ -9,10 +9,18 @@ enterEnvMsg="fridaAnlzAp环境"
 OkEnvMsg="fridaAnlzAp环境 ok: [Miniconda3, neo4j-community, cytoscape]"
 
 echo $enterEnvMsg && \
+#激活
+# 激活 miniconda
 source /app/Miniconda3-py310_22.11.1-1/bin/activate && \
+# 激活 nvm
 source /app/nvm/nvm.sh && \
+# 激活 neo4j
 export NEO4J_HOME=/app/neo4j-community-4.4.32 && \
 export JAVA_HOME=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 && \
 export PATH=$PATH:$NEO4J_HOME/bin:$JAVA_HOME/bin && \
+# 激活 cytoscape
 alias cytoscape_boot_with_java17='JAVA_HOME=/app/zulu17.48.15-ca-jdk17.0.10-linux_x64/ /app/cytoscape-unix-3.10.2/cytoscape.sh' && \
+#启动
+# 启动neo4j
+which neo4j && { neo4j status ||  neo4j start ; } && { neo4j status && echo "启动neoj4j成功" ;} && \
 echo $OkEnvMsg
