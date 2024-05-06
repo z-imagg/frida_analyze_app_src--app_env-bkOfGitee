32116cd7957704798cecabad8a971eae99ec55eb
prgrmz07 <prgrmz07@163.com>
2024-05-06 17:37:40 +0800

    修改模板： 最后  启动bash（以bashrc.sh为rcfile）

--- bashrc.sh
+++ bashrc.sh
@@ -11,4 +11,7 @@ OkEnvMsg="fridaAnlzAp环境 ok: [Miniconda3, neo4j-community, cytoscape]"
 echo $enterEnvMsg && \
 source /app/Miniconda3-py310_22.11.1-1/bin/activate && \
 source /app/nvm/nvm.sh && \
+export NEO4J_HOME=/app/neo4j-community-4.4.32 && \
+export JAVA_HOME=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 && \
+export PATH=$PATH:$NEO4J_HOME/bin:$JAVA_HOME/bin && \
 echo $OkEnvMsg
