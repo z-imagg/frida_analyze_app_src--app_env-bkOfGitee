eab0a5dd27a4836cf45f0a8b8144c7108fb26876
prgrmz07 <prgrmz07@163.com>
2024-05-06 15:17:23 +0800

    修改模板：dkEntry写死脚本名（无需参数）、busz/*.sh、删除busz_run.sh

--- busz_run.sh
+++
@@ -1,7 +0,0 @@
-
-
-
-source $pdir/util/run_shLs_in_dir.sh
-#循环执行业务脚本
-# 业务脚本具有幂等性
-run_shLs_in_dir $pdir/busz
--- util/dkEntry.sh
+++ util/dkEntry.sh
@@ -10,19 +10,17 @@ set -e -u
 
 #导入配置
 source $pdir/docker_instance.sh
+#
+source $pdir/util/run_shLs_in_dir.sh
 
 _importBSFn "argCntEqN.sh"
 
 #docker实例初始化
 function dkEntry() {
 
-
-#断言参数个数为3个
-echo 3 | argCntEqN $* || return $?
-
-local InitProjF=$1
-local BuszRunF=$2
-local manualTxtF=$3
+local InitProjF="$pdir/init_proj.sh"
+local BuszShD="$pdir/busz/"
+local manualTxtF="$pdir/manual_txt.sh"
 
 #'true'为true, 其余都为false
 #  isDkInitProj == docker实例初始化标记
@@ -35,10 +33,10 @@ local flagDone_DkBuszRun=/tmp/flagDone_DkBuszRun_${envNm}
 
 #此次是否应该执行$InitProjF:     从未执行$InitProjF           且    调用者要求执行$InitProjF
 local do_InitProjF=false; ( [[ ! -f $flagDone_InitProj ]] &&  $_isDkInitProj ;) && do_InitProjF=true
-#此次是否应该执行$BuszRunF:     从未执行$BuszRunF             且    调用者要求执行$BuszRunF
+#此次是否应该执行$BuszShD:     从未执行$BuszShD             且    调用者要求执行$BuszShD
 local do_BuszRunF=false;  ( [[ ! -f $flagDone_DkBuszRun  ]] &&  $_isDkBuszRun  ;) && do_BuszRunF=true
 
-#若此次应该执行$InitProjF或$BuszRunF, 则应source此两脚本
+#若此次应该执行$InitProjF或$BuszShD, 则应source此两脚本
 ( $do_InitProjF ||  $do_BuszRunF ;) && { \
 #导入配置
 source $pdir/docker_instance.sh   ;}
@@ -47,8 +45,8 @@ source $pdir/docker_instance.sh   ;}
 ( \
 #若此次应该执行$InitProjF 则执行之       并 设置标记表示 已执行$InitProjF
 { $do_InitProjF && pdir="$pdir" bsFlg="$bsFlg"  bash $bsFlg $InitProjF && touch $flagDone_InitProj && echo "已执行InitProjF:$InitProjF" ;} ; \
-#若此次应该执行$BuszRunF  则执行之       并 设置标记表示 已执行$BuszRunF
-{ $do_BuszRunF  &&  pdir="$pdir" bsFlg="$bsFlg" bash $bsFlg $BuszRunF &&  touch $flagDone_DkBuszRun && echo "已执行BuszRunF:$BuszRunF" ;} ; \
+#若此次应该执行$BuszShD  则执行之       并 设置标记表示 已执行$BuszShD
+{ $do_BuszRunF  &&  run_shLs_in_dir $BuszShD  &&  touch $flagDone_DkBuszRun && echo "已执行BuszShD:$BuszShD" ;} ; \
 true ;) && \
 # 显示 使用手册文本
 bash $manualTxtF && \
