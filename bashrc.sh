73e530ee827aae77191c960ee2d54daf0854a2f8
prgrmz07 <prgrmz07@163.com>
2024-05-10 10:22:12 +0800

    ap-run-anlz项目环境 : neo4j-community三个实例对应qemu8的三个线程

--- bashrc.sh
+++ bashrc.sh
@@ -17,9 +17,33 @@ source /app/Miniconda3-py310_22.11.1-1/bin/activate && \
 # 激活 nvm
 source /app/nvm/nvm.sh && \
 # 激活 neo4j
-export NEO4J_HOME=/app/neo4j-community-4.4.32 && \
-export JAVA_HOME=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 && \
-export PATH=$PATH:$NEO4J_HOME/bin:$JAVA_HOME/bin && \
+function neo4j_inst_new() {
+#【术语】 template == tmpl , inst==instance==neo4j_instance==neo4j实例, cfg==config
+local instIdx=$1
+[[ "X$instIdx" == "X" ]] && return 55
+local neo4j_app_tmpl="/app/neo4j-community-4.4.32"
+local neo4j_app_new="$neo4j_app_tmpl--qemu8Thread${instIdx}"
+cp -r $neo4j_app_tmpl $neo4j_app_tmpl--qemu8Thread${instIdx}
+local portA_tmpl=7474
+local portB_tmpl=7687
+#7474的千位的7减(instIdx+1) ， 比如 7474 --> 6474
+local portA_new=$(( portA_tmpl - (instIdx+1)*1000 ))
+#7687的千位的7减(instIdx+1) ， 比如 7687 --> 6687
+local portB_new=$(( portB_tmpl - (instIdx+1)*1000 ))
+local cfg_tmpl=$neo4j_app_tmpl/conf/neo4j.conf
+local cfg_new=$neo4j_app_new/conf/neo4j.conf
+sed -i -e "s/$portA_tmpl/$portA_new/g" -e "s/$portB_tmpl/$portB_new/g"  $cfg_new && diff $cfg_tmpl  $cfg_new
+}
+#  neo4j-community三个实例对应qemu8的三个线程
+#qemu8Thread0 端口 6474、6687
+neo4j_inst_new 0 && \
+alias neo4j_alias_qemu8Thread0='_neo4jHm=/app/neo4j-community-4.4.32--qemu8Thread0 ; _java11Hm=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 ;    NEO4J_HOME=$_neo4jHm JAVA_HOME=$_java11Hm    PATH=$PATH:$_java11Hm/bin  $_neo4jHm/bin/neo4j'
+#qemu8Thread1 端口 5474、5687
+neo4j_inst_new 1 && \
+alias neo4j_alias_qemu8Thread1='_neo4jHm=/app/neo4j-community-4.4.32--qemu8Thread1 ; _java11Hm=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 ;    NEO4J_HOME=$_neo4jHm JAVA_HOME=$_java11Hm    PATH=$PATH:$_java11Hm/bin  $_neo4jHm/bin/neo4j'
+#qemu8Thread2 端口 4474、4687
+neo4j_inst_new 2 && \
+alias neo4j_alias_qemu8Thread2='_neo4jHm=/app/neo4j-community-4.4.32--qemu8Thread2 ; _java11Hm=/app/zulu11.70.15-ca-jdk11.0.22-linux_x64 ;    NEO4J_HOME=$_neo4jHm JAVA_HOME=$_java11Hm    PATH=$PATH:$_java11Hm/bin  $_neo4jHm/bin/neo4j'
 # 激活 cytoscape
 alias cytoscape_boot_with_java17='JAVA_HOME=/app/zulu17.48.15-ca-jdk17.0.10-linux_x64/ /app/cytoscape-unix-3.10.2/cytoscape.sh' && \
 #启动
