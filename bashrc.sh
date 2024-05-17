cadcd6f5bed51e28b318f589e3b782279e43e049
prgrmz07 <prgrmz07@163.com>
2024-05-17 13:25:48 +0800

    qemu-v8.2.2编译环境:  文案

--- bashrc.sh
+++ bashrc.sh
@@ -5,8 +5,8 @@
 #【术语】 
 #【备注】 
 
-enterEnvMsg="hello-world环境"
-OkEnvMsg="hello-world环境 ok 已具有软件包: [xxx,yyy]"
+enterEnvMsg="qemu-v8.2.2环境"
+OkEnvMsg="qemu-v8.2.2环境 ok 已具有软件包: [xxx,yyy]"
 
 echo $enterEnvMsg && \
 #...
